72165d7187db1c1fca1c610be6329288445faf86
dd_ddong <71020187+dongdong2222@users.noreply.github.com>
2021-11-18 23:55:35 +0900

    ver 4
    전체적으로 다 바꿈
    inputHandler 만들기

--- script.js
+++ script.js
@@ -1,80 +1,125 @@
-class Suject {
-    registObserver(){}
-    deleteObserver(){}
-    notifyObserver(){}
+class UnitTable {
+    constructor (){
+        this.units.push(new InitialUnit(this));
+    }
+    units = [];
+
+    Price = {"currentPurchasePrice": 0,
+            "currentAverageCoinPrice": 0,
+            "numOfCoin": 0
+    }
+    registUnit(unit){
+        this.units.push(unit);
+    }
+    deleteUnit(unit){
+        for(var i=0;i<this.units.length;i++){
+            if(this.units[i] == unit){
+                this.units.slice(i,1);
+            }
+        }
+    }
+    notifyUnit(){
+        for(var i=0;i<this.units.length;i++){
+            this.Price = this.units[i].updateAverageCoinPrice(this.Price);
+        }
+    }
 }
-class Observer{
-    update(){}
+class TotalRegulator {
+    units =[];
+
+    totalPurchase;
+    totalCoinPrice;
+    registUnit(unit){
+        this.units.push(unit);
+    }
+
+    setPurchase(n){
+        this.totalPurchase = n;
+        this.notifyUnit();
+    }
+    setCoinPrice(n) {
+        this.totalcoinPrice = n;
+        this.setPurchase(this.totalpurchase-(this.totalpurchase%this.totalcoinPrice));
+    }
+    notifyUnit(){
+        for(var i=1;i<this.units.length;i++){
+            this.Price = this.units[i].updateSetting(this.totalPurchase, this.totalCoinPrice);
+        }
+    }
 }
 
-class Money {
-    constructor(n){
-        this.price = n;
-    }
-    price;
-    setPrice(n) {
-        this.price = n;
-    }
-    getPrice() {
-        return this.price;
-    }
+class Unit {
 
-}
-class InitialPurchase extends Money{
-    constructor(n){
-        super(n);
+    updateAverageCoinPrice(){
+
     }
-}
-class InitialRevenue extends Money{
-    constructor(n){
-        super(n);
-    }
-}
-class InitialAveragePrice extends Money{
-    constructor(n){
-        super(n);
+    updateSetting(){
+
     }
 }
 
+class InitialUnit extends Unit {
+    constructor(table){
+        super();
+        this.unitTable = table; //set 했을때 notifyUnit을 호출하기 위해
+    }
+    initialPurchasePrice;
+    initialAverageCoinPrice;
 
-class PurchaseUnit { //implement Observer
-    constructor(unitTable) {
-        this.unitTable = unitTable;
-        //this.num = num;
-        this.create();
+    setInitialPurchasePrice(n){ //초기 총평가값 입력시 updateAverage 모두 작동하게
+        this.initialPurchasePrice = n;
+        this.unitTable.notifyUnit();
+    }
+    setInitialAverageCoinPrice(n){
+        this.initialAverageCoinPrice = n;
+        this.unitTable.notifyUnit();
+    }
+    updateAverageCoinPrice(Price){
+        var coin = this.initialPurchasePrice/this.initialAverageCoinPrice;
+        return { "currentPurchasePrice": this.initialPurchasePrice,
+                 "currentAverageCoinPrice": this.initialAverageCoinPrice,
+                 "numOfCoin" : coin};
+    }
+
+}
+
+class PurchaseUnit extends Unit {
+    constructor (table, number) {
+        super();
+        this.unitTable = table;
+        this.number = number;
     }
 
     purchasePrice;
-    priceOfCoin;
-    numofCoin;
-    averagePrice;
+    coinPrice = 1;
+    numOfCoin;
 
-    setPurchasePrice(n){
+    setPurchsePrice(n) {
         this.purchasePrice = n;
-        var input
+        this.numOfCoin = Math.floor(this.purchasePrice/this.coinPrice);
+        this.unitTable.notifyUnit();
     }
-    setPriceOfCoin(n){
-        this.priceOfCoin = n;
-        this.numOfCoin = this.purchasePrice/n;
-        this.purchasePrice = numOfCoin*n;
+    setCoinPrice(n) {
+        this.coinPrice = n;
+        this.setPurchasePrice(this.purchasePrice-(this.purchasePrice%this.coinPrice));
     }
 
-    notifyObserver() {
-        this.unitTable.update(this.currentPurchase, this.currentAveragePrice);
-    }
-    calculateAveragePrice(){
+    updateAverageCoinPrice(Price){
+        var totalPurchase = Price.currentPurchasePrice + this.purchasePrice;
+        var coin = Price.numOfCoin + this.numOfCoin;
+        var totalAverageCoinPrice = totalPurchase/coin;
+        //평단가 화면 업데이트
 
-        var totalNumOfCoin = (this.currentPurchase/currentAveragePrice) + this.numOfCoin;
-        this.currentPurchase += this.purchasePrice;
-        this.currentAveragePrice += this.currentPurchase/ totalNumOfCoin;
-        this.notifyObserver();
-        this.averagePrice = this.currentAveragePrice;
-        this.notifyObserver();
+        return { "currentPurchasePrice": totalPurchase,
+                 "currentAverageCoinPrice": totalAverageCoinPrice,
+                 "numOfCoin" : coin};
+
     }
-    update (purchase, priceOfCoin) {
+    updateSetting(purchase, coinPrice) {
         this.setPurchasePrice(purchase);
-        this.setPriceOfCoin(priceOfCoin);
+        this.setCoinPrice(coinPrice);
     }
+
     create(){
         var parent = document.getElementById("table");
         var addUnit = document.getElementById("add-unit-unit");
@@ -88,6 +133,7 @@ class PurchaseUnit { //implement Observer
         lable.setAttribute("for", "purchase");
         lable.innerText = "매수금";
         input.setAttribute("name", "purchase");
+        input.setAttribute("id", "purchase-price-${this.number}");
         unit.appendChild(lable);
         unit.appendChild(input);
         unit.onchange = this.inputHandler;
@@ -98,6 +144,7 @@ class PurchaseUnit { //implement Observer
         lable.setAttribute("for", "priceOfCoin");
         lable.innerText = "코인가격";
         input.setAttribute("name", "priceOfCoin");
+        input.setAttribute("id", "price-of-coin-${this.number}");
         unit.appendChild(lable);
         unit.appendChild(input);
 
@@ -113,102 +160,11 @@ class PurchaseUnit { //implement Observer
         var button = document.createElement("button");
         button.setAttribute("type", "button");
         button.innerText = "-";
-        button.onclick = ()=>{  this.unitTable.deleteObserver(this);
+        button.onclick = ()=>{  this.unitTable.deleteObserver(this); //손보기
                                 unit.remove();
         };
         unit.appendChild(button);
     }
-    inputHandler(){
-        console.log("")
-    }
-
     
 }
-class PurchaseTable {//implement Subject
-    constructor() {
-        this.remoteCalculate = new RemoteCalculate();
-    }
-    initialPurchase; //초기 매수값
-    initialAveragePrice; //초기
-
-    purchaseUnits = [];
-    currentPurchase;
-    currentAveragePrice;
-
-    totalPurchase;
-    totalPriceofCoin;
-    createUnit(){
-        var unit = new PurchaseUnit(this);
-        var command = new CalculateAverageCommand(unit);
-        this.remoteCalculate.setCommand(command);
-        this.purchaseUnits.push(unit);
-    }
-
-    setCurrentPurchase (n) {
-        this.currentPurchase = n;
-    }
-    setCurrentAveragePrice (n) {
-        this.currentAveragePrice = n;
-    }
-    setInitialValue(a, b){
-        this.initialPurchase = a;
-        this.initialAveragePrice = b;
-        this.currentPurchase = this.initialPurchase;
-        this.currentAveragePrice = this.initialAveragePrice;
-    }
-    registerObserver (ob){
-        this.purchaseUnits.push(ob);
-    }
-    deleteObserver(ob){
-        var i = this.purchaseUnits.indexOf(ob);
-        if(i<0){
-            this.purchaseUnits.slice(i,1);
-        }
-        this.remoteCalculate.deleteCommand(ob);
-    }
-    notifyObserver(){
-        for(var i=0; i<this.purchaseUnits.length;i++){
-            this.purchaseUnits.update(purchase, priceOfCoin);
-        }
-    }
-    update (currentPurchase, currentAveragePrice) {
-        this.currentPurchase = currentPurchase;
-        this.currentAveragePrice = currentAveragePrice;
-    }
-    
-
-}
-
-
-
-class RemoteCalculate {
-    slot = [];
-    setCommand(c){
-        this.slot.push(c);
-    }
-    deleteCommand(c){
-        for(var i=0; i<this.slot.length;i++){
-            if(this.slot[i] === c){
-                this.slot.slice(i,1);
-            }
-        }
-    }
-    calculate(){
-        for(var i=0;i<this.slot.length; i++){
-            this.slot[i].excute();
-        }
-    }
-
-}
-
-class CalculateAverageCommand {
-    purchaseUnit;
-    constructor (unit) {
-        this.purchaseUnit = unit;
-    }
-    execute () {
-        this.purchaseUnit.calculate();
-    }
-}
-var set =new PurchaseTable();
-
+var set = new UnitTable();
